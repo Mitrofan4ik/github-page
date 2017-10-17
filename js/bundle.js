7b6e0eaa2c3bb6796daf60321db7cebce5b67ef4
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-10-17 13:23:50 +0300

    add script to sort list

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,4 @@
-/*FUNCTIONS*/
-
-/*END:FUNCTIONS*/
+// NAVIGATION
 var links = document.getElementsByClassName("-js-goto-innerpage");
 var i;
 for (i = 0; i < links.length; i++) {  
@@ -9,6 +7,7 @@ for (i = 0; i < links.length; i++) {
   });
 }
 
+// NAVIGATION
 var linksBack = document.getElementsByClassName("-js-goto-startpage");
 
 for (var i = 0; i < linksBack.length; i++) {  
@@ -16,3 +15,20 @@ for (var i = 0; i < linksBack.length; i++) {
     document.location.href = "https://mitrofan4ik.github.io/github-page2/";
   });
 }
+
+// SORT LIST
+var dropDown = document.getElementsByClassName("sort-card__title")[0];
+var dropDownType = dropDown.getElementsByTagName("em")[0];
+var dropDownList =document.getElementsByClassName("sort-card-list")[0];
+var dropDownListItems =document.getElementsByClassName("sort-card-list__item");
+var li=0;
+for (li = 0; li < dropDownListItems.length; li++) {  
+  dropDownListItems[li].addEventListener("click", function() {       
+    dropDownType.innerHTML=this.getAttribute('data-sort');
+     dropDownList.classList.toggle('active');
+  });
+}
+
+dropDown.addEventListener("click", function() {    
+  dropDownList.classList.toggle('active');
+});
